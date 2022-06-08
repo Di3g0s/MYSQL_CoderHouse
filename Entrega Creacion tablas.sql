4e95ffe63e7b977fad4c3a212477b916a93b5c34
DiegoAlias <80184331+Di3g0s@users.noreply.github.com>
2022-06-08 00:17:10 -0300

    Add files via upload

--- Entrega Creacion tablas.sql
+++ Entrega Creacion tablas.sql
@@ -1,87 +1,96 @@
+
 CREATE DATABASE MTTO;
 USE MTTO;
 
-select * FROM checklists;
-select * FROM ordenes_de_trabajo;
-
-CREATE TABLE IF NOT EXISTS Checklists (
-	  Id_checklist 		INT NOT NULL 
-	, Id_EQ				INT NOT NULL 
-	, Tipo_de_MTTO		VARCHAR(30) NOT NULL
-	, Descripcion		VARCHAR(50)	NOT NULL
-    , PRIMARY KEY (Id_checklist)
-);
-
-
-CREATE TABLE IF NOT EXISTS MTTO.Notificaciones (
-	  Id_notificacion			INT NOT NULL 
-	, Id_EQ			INT	NOT NULL
-	, Id_checklist		INT
-	, SERVICE_START_DT	DATE 
-	, Componente	VARCHAR(50) 
-	, PRIMARY KEY (Id_notificacion)
-);
-
-
-CREATE TABLE IF NOT EXISTS MTTO.Ordenes_de_trabajo (
-	  Id_orden			INT NOT NULL 
-	, Id_EQ			INT	NOT NULL
-	, Id_notificacion		INT
-	, Descripcion	VARCHAR(60) NOT NULL
-	, Costo	decimal(10,2)
-	, Estado		VARCHAR(50) NOT NULL
-	, PRIMARY KEY (Id_orden)
-);
-
-CREATE TABLE IF NOT EXISTS MTTO.Pendientes_taller (
-	  Id_pendiente			INT NOT NULL 
-	, Id_orden			INT	NOT NULL
-	, Inicio_trabajo		DATETIME
-    , Fin_trabajo DATETIME
-	, Lugar	VARCHAR(60) NOT NULL
-	, Id_tecnico	INT
-    , Id_Supervisor	INT
-	, Comentarios		VARCHAR(50) NOT NULL
-	, PRIMARY KEY (Id_pendiente)
-);
-
-CREATE TABLE IF NOT EXISTS MTTO.Personal (
-	  Id_empleado			INT NOT NULL 
-	, Posicion	VARCHAR(60) NOT NULL
-	, Nombre	VARCHAR(60) NOT NULL
-	, Apellido	VARCHAR(60) NOT NULL
-	, PRIMARY KEY (Id_empleado)
-);
-
-CREATE TABLE IF NOT EXISTS MTTO.Estado_eq (
-	  Id_EQ			INT NOT NULL 
-	, Estado	VARCHAR(60) NOT NULL
-    , primary key (Id_EQ)
-);
-
-Drop table Uso_repuestos;
-
-CREATE TABLE IF NOT EXISTS MTTO.Uso_repuestos (
-	  Id_repuesto			INT NOT NULL 
-	, Id_orden			BIGINT 
-    , Fecha DATE
-    , Cantidad INT
-);
-
-CREATE TABLE IF NOT EXISTS MTTO.Repuestos (
-	  Id_repuesto			INT NOT NULL 
-	, Costo Decimal(10,2)
-    , primary key (id_repuesto)
-);
 
 CREATE TABLE IF NOT EXISTS MTTO.Equipos (
-	  Id_EQ			INT NOT NULL 
+	  id_eq			INT NOT NULL 
 	, Tipo_EQ VARCHAR(30)
 	, Dominio VARCHAR(30)
     , primary key (Id_EQ)
 );
 
+CREATE TABLE IF NOT EXISTS MTTO.JobLog (
+	Pozo_etapa VARCHAR(50),
+	inicio datetime,
+    fin datetime,
+    primary key (Pozo_etapa)
+);
+
+CREATE TABLE IF NOT EXISTS MTTO.Repuestos (
+	id_repuesto INT,
+    Costo DECIMAL(14,2),
+    primary key(id_repuesto)
+);
 
 
+CREATE TABLE IF NOT EXISTS MTTO.Notificaciones (
+	  id_notificacion			INT NOT NULL 
+	, id_eq INT	
+	, SERVICE_START_DT	DATE 
+	, Componente	VARCHAR(50) 
+	, PRIMARY KEY (Id_notificacion)
+    , foreign key (id_eq) references Equipos(id_eq)
+);
+
+CREATE TABLE IF NOT EXISTS MTTO.Ordenes_de_trabajo (
+	  id_orden			INT NOT NULL 
+	, id_eq			INT	NOT NULL references Equipos (id_eq)
+	, id_notificacion		INT
+	, Descripcion	VARCHAR(60) NOT NULL
+	, Costo	decimal(10,2)
+	, Estado		VARCHAR(50) NOT NULL
+	, PRIMARY KEY (Id_orden)
+    , foreign key(id_eq) references Equipos(id_eq)
+    , foreign key(id_notificacion) references Notificaciones(id_notificacion)
+);
 
 
+CREATE TABLE IF NOT EXISTS MTTO.Uso_repuestos (
+	  id_repuesto			INT NOT NULL 
+	, id_orden			INT 
+    , Fecha DATE
+    , Cantidad INT
+    , foreign key (id_repuesto) references Repuestos(id_repuesto)
+    , foreign key (id_orden) references Ordenes_de_trabajo (id_orden)
+);
+
+CREATE TABLE IF NOT EXISTS MTTO.Etapas_Fractura (
+	Pozo_etapa VARCHAR(50) NOT NULL,
+    Pozo VARCHAR(50) NOT NULL,
+    Etapa INT NOT NULL,
+    TiempoBombeo Decimal(14,2),
+	Propante Decimal(14,2),
+	Volumen Decimal(14,2),
+	Prom_Presion Decimal(14,2),
+	Prom_Caudal  Decimal(14,2),
+	Prom_HHP Decimal(14,2),
+    foreign key(Pozo_etapa) references JobLog(Pozo_etapa)
+);
+
+CREATE TABLE IF NOT EXISTS MTTO.BajasEQ (
+	id_baja INT NOT NULL AUTO_INCREMENT,
+    id_eq INT,
+    GrupoTrabajo INT,
+    TipoBaja VARCHAR(50),
+    Componente VARCHAR(50),
+    Motivo VARCHAR(150),
+    Locacion VARCHAR(50),
+    PRIMARY KEY (id_baja),
+    FOREIGN KEY (id_eq) REFERENCES Equipos(id_eq)
+);
+
+CREATE TABLE IF NOT EXISTS 	PruebaEquipo (
+	id_orden INT,
+    id_eq INT,
+    FechaHora_inicio datetime,
+    FechaHora_fin datetime,
+    Vuelve_Taller decimal(14,2),
+    Comentarios Varchar(150),
+    PRIMARY KEY (id_orden),
+    FOREIGN KEY (id_eq) REFERENCES Equipos(id_eq)
+);
+    
+
+
+-- select * FROM Ordenes_de_trabajo;
